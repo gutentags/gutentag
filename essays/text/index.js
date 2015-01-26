e6cf42ae261a8d928d35972977fc7e7c8412695e
Kris Kowal <kris.kowal@cixar.com>
2015-01-25 22:35:50 -0800

    Implement text component

---
+++ essays/text/index.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var Document = require("../../dom-body");
+var Scope = require("../../scope");
+var Essay = require("./essay.html");
+
+var scope = new Scope();
+var document = new Document(window.document.body);
+var essay = new Essay(document.documentElement, scope);
+essay.greeting.value = "Guten Tag, Welt!";
