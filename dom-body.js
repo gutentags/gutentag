b5e03fd69e8eb0140f0af84de57e52cb4d163b73
Kris Kowal <kris.kowal@cixar.com>
2015-01-25 17:35:44 -0800

    Draft of proxy DOM with bodies.

---
+++ dom-body.js
@@ -0,0 +1,179 @@
+"use strict";
+
+var BaseDocument = require("./dom");
+var BaseNode = BaseDocument.prototype.Node;
+var BaseElement = BaseDocument.prototype.Element;
+var BaseTextNode = BaseDocument.prototype.TextNode;
+
+module.exports = Document;
+function Document(actualNode) {
+    Node.call(this, this);
+    this.actualNode = actualNode;
+    this.actualDocument = actualNode.ownerDocument;
+
+    this.documentElement = this.createBody();
+    this.documentElement.parentNode = this;
+    actualNode.appendChild(this.documentElement.actualNode);
+
+    this.firstChild = this.documentElement;
+    this.lastChild = this.documentElement;
+}
+
+Document.prototype = Object.create(BaseDocument.prototype);
+Document.prototype.Node = Node;
+Document.prototype.Element = Element;
+Document.prototype.TextNode = TextNode;
+Document.prototype.Body = Body;
+
+Document.prototype.createBody = function () {
+    return new this.Body(this);
+};
+
+Document.prototype.getActualElement = function () {
+    return this.actualNode;
+};
+
+function Node(document) {
+    BaseNode.call(this, document);
+    this.actualNode = null;
+}
+
+Node.prototype = Object.create(BaseNode.prototype);
+Node.prototype.constructor = Node;
+
+Node.prototype.insertBefore = function insertBefore(childNode, nextSibling) {
+    if (nextSibling && nextSibling.parentNode !== this) {
+        throw new Error("Can't insert before node that is not a child of parent");
+    }
+    BaseNode.prototype.insertBefore.call(this, childNode, nextSibling);
+    var actualNextSibling = nextSibling && nextSibling.getActualFirstNode();
+    this.getActualElement().insertBefore(childNode.actualNode, actualNextSibling || null);
+    childNode.inject();
+    return childNode;
+};
+
+Node.prototype.removeChild = function removeChild(childNode) {
+    childNode.extract();
+    this.getActualElement().removeChild(childNode.actualNode);
+    BaseNode.prototype.removeChild.call(this, childNode);
+};
+
+Node.prototype.inject = function injectNode() { };
+
+Node.prototype.extract = function extractNode() { };
+
+Node.prototype.getActualElement = function () {
+    return this.actualNode;
+};
+
+Node.prototype.getActualFirstNode = function () {
+    return this.actualNode;
+};
+
+Object.defineProperty(Node.prototype, "innerHTML", {
+    get: function () {
+        return this.actualNode.innerHTML;
+    }
+});
+
+function Element(document, type) {
+    BaseNode.call(this, document);
+    this.tagName = type;
+    this.actualNode = document.actualDocument.createElement(type);
+}
+
+Element.prototype = Object.create(Node.prototype);
+Element.prototype.constructor = Element;
+Element.prototype.nodeType = 1;
+
+function TextNode(document, text) {
+    Node.call(this, document);
+    this.actualNode = document.actualDocument.createTextNode(text);
+}
+
+Object.defineProperty(TextNode.prototype, "data", {
+    set: function (data) {
+        this.actualNode.data = data;
+    },
+    get: function () {
+        return this.actualNode.data;
+    }
+});
+
+TextNode.prototype = Object.create(Node.prototype);
+TextNode.prototype.constructor = TextNode;
+TextNode.prototype.nodeType = 3;
+
+// if parentNode is null, the body is extracted
+// if parentNode is non-null, the body is inserted
+function Body(document) {
+    Node.call(this, document);
+    this.actualNode = document.actualDocument.createTextNode("");
+    this.actualFirstChild = null;
+    this.actualBody = document.actualDocument.createElement("NOBODY");
+}
+
+Body.prototype = Object.create(Node.prototype);
+Body.prototype.constructor = Body;
+Body.prototype.nodeType = 13;
+
+Body.prototype.extract = function extract() {
+    var body = this.actualBody;
+    var lastChild = this.actualNode;
+    var parentNode = this.parentNode.getActualElement();
+    var at = this.getActualFirstNode();
+    var next;
+    while (at && at !== lastChild) {
+        next = at.nextSibling;
+        if (body) {
+            body.appendChild(at);
+        } else {
+            parentNode.removeChild(at);
+        }
+        at = next;
+    }
+};
+
+Body.prototype.inject = function inject() {
+    if (!this.parentNode) {
+        throw new Error("Can't inject without a parent node");
+    }
+    var body = this.actualBody;
+    var lastChild = this.actualNode;
+    var parentNode = this.parentNode.getActualElement();
+    var at = body.firstChild;
+    var next;
+    while (at) {
+        next = at.nextSibling;
+        parentNode.insertBefore(at, lastChild);
+        at = next;
+    }
+};
+
+Body.prototype.getActualElement = function () {
+    if (this.parentNode) {
+        return this.parentNode.getActualElement();
+    } else {
+        return this.actualBody;
+    }
+};
+
+Body.prototype.getActualFirstNode = function () {
+    if (this.firstChild) {
+        return this.firstChild.getActualFirstNode();
+    }
+};
+
+Object.defineProperty(Body.prototype, "innerHTML", {
+    get: function () {
+        if (this.parentNode) {
+            this.extract();
+            var html = this.actualBody.innerHTML;
+            this.inject();
+            return html;
+        } else {
+            return this.actualBody.innerHTML;
+        }
+    }
+});
+
---
+++ dom.js
@@ -0,0 +1,109 @@
+"use strict";
+
+module.exports = Document;
+function Document() {
+    this.documentElement = null;
+}
+
+Document.prototype.Node = Node;
+Document.prototype.Element = Element;
+Document.prototype.TextNode = TextNode;
+
+Document.prototype.createTextNode = function (text) {
+    return new this.TextNode(this, text);
+};
+
+Document.prototype.createElement = function (type) {
+    return new this.Element(this, type);
+};
+
+function Node(document) {
+    this.ownerDocument = document;
+    this.parentNode = null;
+    this.firstChild = null;
+    this.lastChild = null;
+    this.previousSibling = null;
+    this.nextSibling = null;
+}
+
+Node.prototype.appendChild = function appendChild(childNode) {
+    return this.insertBefore(childNode, null);
+};
+
+Node.prototype.insertBefore = function insertBefore(childNode, nextSibling) {
+    if (!childNode) {
+        throw new Error("Can't insert null child");
+    }
+    if (childNode.ownerDocument !== this.ownerDocument) {
+        throw new Error("Can't insert child from foreign document");
+    }
+    if (childNode.parentNode) {
+        childNode.parentNode.removeChild(childNode);
+    }
+    var previousSibling;
+    if (nextSibling) {
+        previousSibling = nextSibling.previousSibling;
+    } else {
+        previousSibling = this.lastChild;
+    }
+    if (previousSibling) {
+        previousSibling.nextSibling = childNode;
+    }
+    if (nextSibling) {
+        nextSibling.previousSibling = childNode;
+    }
+    childNode.nextSibling = nextSibling;
+    childNode.previousSibling = previousSibling;
+    childNode.parentNode = this;
+    if (!nextSibling) {
+        this.lastChild = childNode;
+    }
+    if (!previousSibling) {
+        this.firstChild = childNode;
+    }
+};
+
+Node.prototype.removeChild = function removeChild(childNode) {
+    if (!childNode) {
+        throw new Error("Can't remove null child");
+    }
+    var parentNode = childNode.parentNode;
+    if (parentNode !== this) {
+        throw new Error("Can't remove node that is not a child of parent");
+    }
+    if (childNode === parentNode.firstChild) {
+        parentNode.firstChild = childNode.nextSibling;
+    }
+    if (childNode === parentNode.lastChild) {
+        parentNode.lastChild = childNode.previousSibling;
+    }
+    if (childNode.previousSibling) {
+        childNode.previousSibling.nextSibling = childNode.nextSibling;
+    }
+    if (childNode.nextSibling) {
+        childNode.nextSibling.previousSibling = childNode.previousSibling;
+    }
+    childNode.previousSibling = null;
+    childNode.parentNode = null;
+    childNode.nextSibling = null;
+    return childNode;
+};
+
+function TextNode(document, text) {
+    Node.call(this, document);
+    this.data = text;
+}
+
+TextNode.prototype = Object.create(Node.prototype);
+TextNode.prototype.constructor = TextNode;
+TextNode.prototype.nodeType = 3;
+
+function Element(document, type) {
+    Node.call(this, document);
+    this.tagName = type;
+}
+
+Element.prototype = Object.create(Node.prototype);
+Element.prototype.constructor = Element;
+Element.prototype.nodeType = 1;
+
