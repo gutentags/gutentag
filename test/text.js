0aaa800dbf7c159eb4d238765d5a65b829c8b4b6
Kris Kowal <kris.kowal@cixar.com>
2015-04-21 20:59:52 -0700

    Uprev domenic, koerper, ultimately minidom, and fix test

--- test/text.js
+++ test/text.js
@@ -6,14 +6,15 @@ var Essay = require("./text.html");
 var Document = require("koerper");
 
 var parser = new DOMParser();
-var actualDocument = parser.parseFromString("<!doctype html><html><body></body></html>", "text/html");
-var document = new Document(actualDocument.firstChild.firstChild);
+var actualDocument = parser.parseFromString("<!doctype html><html><head></head><body></body></html>", "text/html");
+var body = actualDocument.firstChild.nextSibling.firstChild.nextSibling;
+var document = new Document(body);
 var scope = new Scope();
 var essay = new Essay(document.documentElement, scope);
 essay.scope.components.greeting.value = "Guten Tag, Welt!";
 
-expect(actualDocument.firstChild.innerHTML).toBe("<body>Guten Tag, Welt!</body>");
+expect(body.innerHTML).toBe("Guten Tag, Welt!");
 
 essay.scope.components.greeting.value = "Auf Wiederseh'n!";
 
-expect(actualDocument.firstChild.innerHTML).toBe("<body>Auf Wiederseh'n!</body>");
+expect(body.innerHTML).toBe("Auf Wiederseh'n!");
