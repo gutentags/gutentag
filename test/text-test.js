2660d425652bd2681f4c25da9a7c55565eab80f3
Kris Kowal <kris@uber.com>
2015-09-07 10:12:12 -0700

    Fix text test

--- test/text-test.js
+++ test/text-test.js
@@ -6,7 +6,7 @@ var URL = require("url");
 
 it("", function () {
     var location = Location.fromDirectory(__dirname);
-    var packageLocation = URL.resolve(location, "..");
+    var packageLocation = URL.resolve(location, "../");
     return System.load(packageLocation)
     .then(function (system) {
         return system.import("./test/text");
